e3144d4ed51b4bf8ea13c2b41a640bdb084c3c43
Matthew James <matthewsalgorithm@gmail.com>
2025-06-22 23:56:52 +1000

    WIP

--- src/debug.sh
+++ src/debug.sh
@@ -6,6 +6,7 @@ valgrind -s --leak-check=full --show-leak-kinds=all ../build/base64 Man
 echo ""
 echo "Running test cases."
 tests=("Man" "Ma" "M" "Many hands make light work." "light work." "light work" "light wor" "light wo" "light w")
+expected=("TWFu" "TWE=" "TQ==" "TWFueSBoYW5kcyBtYWtlIGxpZ2h0IHdvcmsu" "bGlnaHQgd29yay4=" "bGlnaHQgd29yaw==" "bGlnaHQgd29y" "bGlnaHQgd28=" "bGlnaHQgdw==")
 
 for test in "${tests[@]}"; do
     echo ""
