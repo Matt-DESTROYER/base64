08135a55535581cd095cc426074ebabf52ce47f4
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2025-06-30 12:45:33 +1000

    Use `C_COMPILER`

--- src/build.sh
+++ src/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ -z "${C_COMPILER:-}" ]; then
+	export C_COMPILER="gcc"
+fi
+
 . ./detect-runner.sh
 
 if [ "$OSTYPE" = "Windows" ]; then
@@ -8,4 +12,4 @@ fi
 
 mkdir -p ../build/$RUNNER_OS/$ARCH
 
-gcc ./main.c ./base64.c -o ../build/$RUNNER_OS/$ARCH/base64$EXT -Wall -Wextra -pedantic
+$C_COMPILER ./main.c ./base64.c -o ../build/$RUNNER_OS/$ARCH/base64$EXT -Wall -Wextra -pedantic
